1f61fd02c517f4e4a3f0518c07c81f1b18bb8513
Ivan Rivas <IRivas1@my.gcu.edu>
2019-03-07 14:11:59 -0700

    Add project files.

---
+++ IvanRivas_Milestone2CST117/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IvanRivas_Milestone2CST117
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
