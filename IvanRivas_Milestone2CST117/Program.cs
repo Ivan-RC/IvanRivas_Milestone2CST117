488f4145cfb76fa7fecb91bb73582ea6b3c3d89e
Ivan Rivas <IRivas1@my.gcu.edu>
2019-03-08 14:48:01 -0700

    Update

--- IvanRivas_Milestone2CST117/Program.cs
+++ IvanRivas_Milestone2CST117/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+/// <summary>
+/// Ivan Rivas
+/// This is my own work. With the help of Ms. Fritz.
+/// </summary>
 
 namespace IvanRivas_Milestone2CST117
 {
@@ -10,6 +14,41 @@ namespace IvanRivas_Milestone2CST117
     {
         static void Main(string[] args)
         {
+            movieInventory movie = new movieInventory("Spiderman", 1, 15.00, "Fantasy/Action", 2002, 5);
+            //Testing getters
+            Console.WriteLine("The movie is " + movie.getTitle() + ". It's ID is " + movie.getID() + ". The price of the movie is " + movie.getPrice() +
+               " dollars. The genre is " + movie.getGenre() + ". It was released " + movie.getreleaseDate() + ". We only have " + movie.getQnty() + " in stock.");
+
+            //Testing setters
+            Console.WriteLine("Changing movie name...");
+            movie.setTitle("The Conjuring 2");
+            Console.WriteLine("The name of the movie is now " + movie.getTitle() + ".");
+
+            Console.WriteLine("Changing movie ID...");
+            movie.setID(6);
+            Console.WriteLine("The ID of the movie is now " + movie.getID() + ".");
+
+            Console.WriteLine("Changing movie price...");
+            movie.setPrice(60.00);
+            Console.WriteLine("The price of the movie is now $" + movie.getPrice() + ".");
+
+            Console.WriteLine("Changing movie genre...");
+            movie.setGenre("Horror");
+            Console.WriteLine("The genre of the movie is now " + movie.getGenre() + ".");
+
+            Console.WriteLine("Changing movie release date...");
+            movie.setreleaseDate(2016);
+            Console.WriteLine("The release date of the movie is now " + movie.getreleaseDate() + ".");
+
+            Console.WriteLine("Changing the stock quantity...");
+            movie.setQnty(50);
+            Console.WriteLine("We now have " + movie.getQnty() + " in stock.");
+
+            Console.WriteLine("The movie is " + movie.getTitle() + ". It's ID is " + movie.getID() + ". The price of the movie is " + movie.getPrice() +
+               " dollars. The genre is " + movie.getGenre() + ". It was released " + movie.getreleaseDate() + ". We only have " + movie.getQnty() + " in stock.");
+
+
+
         }
     }
 }
---
+++ IvanRivas_Milestone2CST117/movieInventory.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// Ivan Rivas
+/// This is my own work. With the help of Ms. Fritz.
+/// </summary>
+
+namespace IvanRivas_Milestone2CST117
+{
+    class movieInventory
+    {
+        //Fields 
+        private string title;
+        private int prodID;
+        private double price;
+        private string genre;
+        private int releaseDate;
+        private int stockQnty;
+
+        //constructor
+        public movieInventory(string movieName, int id, double moviePrice, string movieGenre, int movieReleaseDate, int moviesQnty)
+        {
+            this.title = movieName;
+            this.prodID = id;
+            this.price = moviePrice;
+            this.genre = movieGenre;
+            this.releaseDate = movieReleaseDate;
+            this.stockQnty = moviesQnty;
+        }
+
+        //getters
+        public string getTitle() { return title; }
+        public int getID() { return prodID;  }
+        public double getPrice() { return price; }
+        public string getGenre() { return genre; }
+        public int getreleaseDate() { return releaseDate; }
+        public int getQnty() { return stockQnty; }
+
+        //setters 
+        public void setTitle(string newTitle) { title = newTitle; }
+        public void setID(int newID) { prodID = newID;  }
+        public void setPrice(double newPrice) { price = newPrice; }
+        public void setGenre(string newGenre) { genre = newGenre;  }
+        public void setreleaseDate(int newreleaseDate) { releaseDate = newreleaseDate; }
+        public void setQnty(int newQnty) { stockQnty = newQnty;}
+    }
+}
